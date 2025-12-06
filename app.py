d9bfdc38f14fbc421b6d74d6be0b7a637a0fddf7
Mayank Sejwar <mayanksejwar58@gmail.com>
2025-12-06 12:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 client=genai.Client(api_key='AIzaSyDmQB_ncrzoOMnSd9teArjafaWJnh0Kdyk')
 @app.route('/')
 def home():
+    return redirect(url_for('welcome'))
+
+@app.route('/welcome')
+def welcome():
     return render_template('welcome.html')
 
 @app.route('/chatp')
